5ca708b70b4c2809710c345311165553a5fbe633
PPPPP <32994395+P-PPPP@users.noreply.github.com>
2022-04-08 08:58:41 +0800

    Debug

--- components/video_Down.py
+++ components/video_Down.py
@@ -12,17 +12,25 @@ def bilibili(bv:str,ASDB:bool=False,download_sourcer:int=0):
     # 2: 音频
     headers = {"User-Agent":"Mozilla/5.0"}
     if "bilibili" in bv: bv = bv.split("/")[-1].split("?")[0]
-
-    if ASDB: cids = [fn["cid"] for fn in requests.get(f"https://api.bilibili.com/x/web-interface/view?bvid={bv}",headers=headers).json()["data"]["pages"]
+    pages = requests.get(f"https://api.bilibili.com/x/web-interface/view?bvid={bv}",headers=headers).json()["data"]["pages"]
+    pages.encoding = 'utf-8'
+    if ASDB: cids = [fn["cid"] for fn in pages
             if "弹幕" not in fn["part"]]
-    else: cids = [fn["cid"] for fn in requests.get(f"https://api.bilibili.com/x/web-interface/view?bvid={bv}",headers=headers).json()["data"]["pages"]]
+    else: cids = [fn["cid"] for fn in pages]
     os.system(f"echo Start Download For {bv} with {cids}")
     for i in cids:
         if download_sourcer == 0:
-            download_url = requests.get(f"https://api.bilibili.com/x/player/playurl?bvid={bv}&cid={i}&otype=json&&platform=html5&high_quality=0",headers=headers).json()["data"]["durl"][0]["url"]
-            print(requests.get(f"https://api.bilibili.com/x/player/playurl?bvid={bv}&cid={i}&qn=16",headers=headers).json())
+            download_url = requests.get(f"https://api.bilibili.com/x/player/playurl?bvid={bv}&cid={i}&otype=json&&platform=html5&high_quality=0",headers=headers)
+            download_url.encoding='utf-8'
+            print(download_url)
+            download_url = download_url.json()["data"]["durl"][0]["url"]
         if download_sourcer == 1:
-            download_url = requests.get(f"https://api.bilibili.com/x/player/playurl?bvid={bv}&cid={i}&qn=16",headers=headers).json()["data"]["durl"][0]["url"]
+            download_url = requests.get(f"https://api.bilibili.com/x/player/playurl?bvid={bv}&cid={i}&qn=16",headers=headers)
+            download_url.encoding='utf-8'
+            download_url = download_url.json()["data"]["durl"][0]["url"]
         if download_sourcer == 2:
-            download_url = requests.get(f"https://api.bilibili.com/x/player/playurl?bvid={bv}&cid={i}&qn=0&fnver=0&fnval=80",headers=headers).json()["data"]["dash"]["audio"][0]["baseUrl"]
+            download_url = requests.get(f"https://api.bilibili.com/x/player/playurl?bvid={bv}&cid={i}&qn=0&fnver=0&fnval=80",headers=headers)
+            download_url.encoding='utf-8'
+            download_url = download_url.json()["data"]["dash"]["audio"][0]["baseUrl"]
+
         aria2(url=download_url,name=f"{bv}-{cids.index(i)+1}.mp4" if len(cids) >1 else f"{bv}.mp4",args=' --header="Refer:https://www.bilibili.com" --user-agent="Mozilla/5.0" ')

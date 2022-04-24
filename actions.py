da019c826aa76673508fb71d2cd969dab77dd8b4
PPPPP <32994395+P-PPPP@users.noreply.github.com>
2022-04-24 22:27:32 +0800

    DEBUG

--- actions.py
+++ actions.py
@@ -46,8 +46,8 @@ class Actions:
     @Start_Func
     def Install_JianYing(self):
         os.mkdir("./components/tmp") if not os.path.exists("./components/tmp") else None
-        subprocess.run("choco install -y ffmpeg aria2 7zip",shell=False
-            ,stdout=subprocess.DEVNULL,stderr=subprocess.DEVNULL)
+        #subprocess.run("choco install -y ffmpeg aria2 7zip",shell=False
+        #    ,stdout=subprocess.DEVNULL,stderr=subprocess.DEVNULL)
         subprocess.Popen(f"aria2c  -x 16 -s 16 -k 1M -o ./_tmp.exe {Config['Jy_Download_Url']}",shell=False,stdout=subprocess.DEVNULL,stderr=subprocess.DEVNULL).wait()
         os.system(f"echo Finish Install Dependiencies")
         install_process = subprocess.Popen("_tmp.exe",shell=True)

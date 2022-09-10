8b339f1ed108a7be909e14fd769ff5293248c418
PPPPP <ppzzhh1186861238@hotmail.com>
2022-09-10 12:48:22 +0800

    debug

--- main.py
+++ main.py
@@ -76,7 +76,6 @@ if __name__ == "__main__":
     if "JianYing_Path" in Config["Basic"] and Config["Basic"]["JianYing_Path"] != "": _ins = Api.Jy_Warp.Instance(Start_Jy=True,JianYing_Exe_Path=os.path.join(Config["Basic"]["JianYing_Path"],"Apps","JianyingPro.exe"))
     else: _ins = Api.Jy_Warp.Instance(Start_Jy=True) # Default Path
     Api.Logic_warp.echo("Creat Main Instance.")
-    Api.Logic_warp.echo(_ins._detect_viewport())
     _ins._Start_New_Draft_Content(wait=True) #进入主页面
 
     # 准备媒体文件
